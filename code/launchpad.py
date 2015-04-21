ec9c803d3eddf43333b145d5c93cbb6bc457e916
pollyl <pollyl@mit.edu>
2015-04-21 19:08:28 -0400

    added graphical UI images only. No functionality in place

--- code/launchpad.py
+++ code/launchpad.py
@@ -22,11 +22,59 @@ from synth import *
 import rtmidi_python as rtmidi
 
 from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.clock import Clock as kivyClock
+
+from kivy.core.window import Window
+from random import random
+
+from kivy.graphics.instructions import InstructionGroup
+from kivy.graphics import Color, Ellipse, Rectangle, Line
+from kivy.graphics import PushMatrix, PopMatrix, Translate, Scale, Rotate
 from kivy.clock import Clock as kivyClock
 
 import numpy as np
 import threading
 import time
+
+
+# Class that contains instructions for UI graphics components (record button, instrument images, etc.)
+class UIGraphics(InstructionGroup):
+   def __init__(self):
+      super(UIGraphics, self).__init__()
+      # record button and instrument select box
+      self.record_circle_color = Color(*(1, 1, 1), mode='hsv')
+      self.record_circle = Ellipse(segments = 40, pos = (650, 50), size=(110, 110))
+      self.instrument_select_points = [40, 40, 180, 40, 180, 180, 40, 180, 40, 40]
+      self.instrument_select =  Line(points=self.instrument_select_points, width=5)
+
+      # instruments
+      self.instrument_color = Color(*(0, 0, 1), mode='hsv')
+      self.guitar = Rectangle(source="./images/guitar.png", pos=(50, 50), size=(120, 120))
+      self.keyboard = Rectangle(source="./images/keyboard.png", pos=(200, 50), size=(120, 120))
+      self.drum = Rectangle(source="./images/drum.png", pos=(350, 50), size=(120, 120))
+      self.bass = Rectangle(source="./images/bass.png", pos=(500, 50), size=(120, 120))
+
+   def show(self):
+      self.add(self.record_circle_color)
+      self.add(self.instrument_select)
+      self.add(self.record_circle)
+      self.add(self.instrument_color)
+      self.add(self.guitar)
+      self.add(self.keyboard)
+      self.add(self.drum)
+      self.add(self.bass)
+
+   def hide(self):
+      self.remove(self.record_circle_color)
+      self.remove(self.record_circle)
+      self.remove(self.instrument_select)
+      self.remove(self.instrument_color)
+      self.remove(self.keyboard)
+      self.remove(self.guitar)
+      self.remove(self.keyboard)
+      self.remove(self.drum)
+      self.remove(self.bass)
 
 
 class MainWidget(BaseWidget) :
@@ -36,14 +84,13 @@ class MainWidget(BaseWidget) :
       # basic audio / synth
       self.audio = Audio()
       self.synth = Synth('../FluidR3_GM.sf2')
-      self.synth.program(0 , 128, 0)
       self.audio.add_generator(self.synth)
 
       # song with Step sequencer as a track
       self.song = Song()
       #self.song.add_track( Metronome( self.synth ) )
-      #self.step = StepSequencer(self.synth, (35, 40, 42), 8, self.on_step_callback)
-      #self.song.add_track( self.step )
+      self.step = StepSequencer(self.synth, (35, 40, 42), 8, self.on_step_callback)
+      self.song.add_track( self.step )
 
       # tempo
       self.song.cond.set_bpm(120)
@@ -54,13 +101,21 @@ class MainWidget(BaseWidget) :
       self.add_widget(self.info)
 
       # midi setup
-      self.midi_out = open_midi_out("Launchpad")
+      # self.midi_out = open_midi_out("Launchpad")
       self.midi_in = open_midi_in("Launchpad", self.on_midi_in)
 
       # thread setup for higher-resolution song polling
       self.thread = myThread(self.song)
       self.thread.start()
       core.register_terminate_func(self.thread.kill)
+
+
+      #-------- Graphics --------#
+      self.UIGraphics = UIGraphics()
+      self.canvas.add(self.UIGraphics)
+      self.UIGraphics.show()
+
+
 
    def on_update(self) :
       self.info.text = "%d fps\naudio:%s" % (kivyClock.get_fps(), self.audio.get_load())
@@ -93,17 +148,9 @@ class MainWidget(BaseWidget) :
 
    # incoming midi message (on a separate thread)
    def on_midi_in(self, message, time_stamp):
-      hex_msg = []
-      for x in message:
-         hex_msg.append(hex(x))
-
-      if len(message) < 3:
-         return
-
       print message
-
-      if message[2] > 0:
-         self.synth.noteon(0, message[1], 100)
+      if len(message) == 0:
+         return
 
       cmd, key, vel = message
 
@@ -245,6 +292,7 @@ def open_midi_in(name, callback):
       midi_in.callback = callback
       return midi_in
    raise Exception ("Error: Can't find Midi In port with name " + name)
+
 
 
 run(MainWidget)
