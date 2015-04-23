5846ed999d9525a394c95a11ce17c071dc9f0db9
pollyl <pollyl@mit.edu>
2015-04-23 00:53:29 -0400

    instrument selection using buttons works

--- code/launchpad.py
+++ code/launchpad.py
@@ -42,39 +42,46 @@ import time
 class UIGraphics(InstructionGroup):
    def __init__(self):
       super(UIGraphics, self).__init__()
-      # record button and instrument select box
+      # record button 
       self.record_circle_color = Color(*(1, 1, 1), mode='hsv')
-      self.record_circle = Ellipse(segments = 40, pos = (650, 50), size=(110, 110))
-      self.instrument_select_points = [40, 40, 180, 40, 180, 180, 40, 180, 40, 40]
-      self.instrument_select =  Line(points=self.instrument_select_points, width=5)
+      self.record_circle = Ellipse(segments = 40, pos = (55, 20), size=(70, 70))
+
+      # instrument select box
+      self.select_box_points = [[45, 375, 135, 375, 135, 465, 45, 465, 45, 375], [45, 285, 135, 285, 135, 375, 45, 375, 45, 285], [45, 195, 135, 195, 135, 285, 45, 285, 45, 195], [45, 105, 135, 105, 135, 195, 45, 195, 45, 105]]
+      self.select_box =  Line(points=self.select_box_points, width=5)
 
       # instruments
       self.instrument_color = Color(*(0, 0, 1), mode='hsv')
-      self.guitar = Rectangle(source="./images/guitar.png", pos=(50, 50), size=(120, 120))
-      self.keyboard = Rectangle(source="./images/keyboard.png", pos=(200, 50), size=(120, 120))
-      self.drum = Rectangle(source="./images/drum.png", pos=(350, 50), size=(120, 120))
-      self.bass = Rectangle(source="./images/bass.png", pos=(500, 50), size=(120, 120))
+      self.instruments = [Rectangle(source="./images/guitar.png", pos=(50, 380), size=(80, 80)), Rectangle(source="./images/keyboard.png", pos=(50, 290), size=(80, 80)), Rectangle(source="./images/drum.png", pos=(50, 200), size=(80, 80)), Rectangle(source="./images/bass.png", pos=(50, 110), size=(80, 80))]
+      self.instrument_index = 0
+
 
    def show(self):
       self.add(self.record_circle_color)
-      self.add(self.instrument_select)
+      self.select_box.points = self.select_box_points[self.instrument_index]
+      self.add(self.select_box)
       self.add(self.record_circle)
       self.add(self.instrument_color)
-      self.add(self.guitar)
-      self.add(self.keyboard)
-      self.add(self.drum)
-      self.add(self.bass)
+      for instrument in self.instruments:
+         self.add(instrument)
+
 
    def hide(self):
       self.remove(self.record_circle_color)
       self.remove(self.record_circle)
-      self.remove(self.instrument_select)
+      self.remove(self.select_box)
       self.remove(self.instrument_color)
-      self.remove(self.keyboard)
-      self.remove(self.guitar)
-      self.remove(self.keyboard)
-      self.remove(self.drum)
-      self.remove(self.bass)
+      for instrument in self.instruments:
+         self.remove(instrument)
+
+
+   def set_instrument_index(self, index):
+      self.instrument_index = index
+      self.select_box.points = self.select_box_points[self.instrument_index]
+
+   def get_instrument_index(self):
+      return self.instrument_index
+
 
 
 class MainWidget(BaseWidget) :
@@ -86,11 +93,17 @@ class MainWidget(BaseWidget) :
       self.synth = Synth('../FluidR3_GM.sf2')
       self.audio.add_generator(self.synth)
 
+      # midi instrument settings
+      self.instrument_i = 0
+      self.midi_instruments = [(0, 0, 27), (0, 0, 1), (0, 128, 0), (0, 0, 36)]
+      self.synth.program(*self.midi_instruments[self.instrument_i])
+      self.button_number = 27
+
       # song with Step sequencer as a track
       self.song = Song()
       #self.song.add_track( Metronome( self.synth ) )
-      self.step = StepSequencer(self.synth, (35, 40, 42), 8, self.on_step_callback)
-      self.song.add_track( self.step )
+      # self.step = StepSequencer(self.synth, (35, 40, 42), 8, self.on_step_callback)
+      # self.song.add_track( self.step )
 
       # tempo
       self.song.cond.set_bpm(120)
@@ -101,7 +114,7 @@ class MainWidget(BaseWidget) :
       self.add_widget(self.info)
 
       # midi setup
-      # self.midi_out = open_midi_out("Launchpad")
+      self.midi_out = open_midi_out("Launchpad")
       self.midi_in = open_midi_in("Launchpad", self.on_midi_in)
 
       # thread setup for higher-resolution song polling
@@ -146,11 +159,37 @@ class MainWidget(BaseWidget) :
          # full reset of launchpad
          self.midi_out.send_message([176, 0, 0 ])
 
+
    # incoming midi message (on a separate thread)
    def on_midi_in(self, message, time_stamp):
-      print message
-      if len(message) == 0:
+      hex_msg = []
+      for x in message:
+         hex_msg.append(hex(x))
+      
+      if len(message) < 1:
          return
+      
+      print message
+      
+      # use green and red buttons to change instrument selection
+      if len(message) == 2:
+         instrument_i = self.UIGraphics.get_instrument_index()
+         if message[1] > self.button_number:
+            if message[0] == 197:
+               self.instrument_i = (instrument_i - 1) % 4
+               self.UIGraphics.set_instrument_index(self.instrument_i)
+               self.button_number = message[1]
+               self.synth.program(*self.midi_instruments[self.instrument_i])
+         elif message[1] < self.button_number:
+            if message[0] == 197:
+               self.instrument_i = (instrument_i + 1) % 4
+               self.UIGraphics.set_instrument_index(self.instrument_i)
+               self.button_number = message[1]
+               self.synth.program(*self.midi_instruments[self.instrument_i])
+         return
+
+      if message[2] > 0:
+         self.synth.noteon(0, message[1], 100)
 
       cmd, key, vel = message
 
